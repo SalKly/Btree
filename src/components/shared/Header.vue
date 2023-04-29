27715a40b183d777aa3bc3a6b437b64b844c2d88
@salem52 <mohamed7arkat@outlook.com>
2023-04-29 20:32:55 +0300

    .

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -5,23 +5,7 @@
     tile
     height="80px"
   >
-    <v-toolbar
-      height="80px"
-    >
-      <!-- <v-app-bar-nav-icon /> -->
-      <!-- <v-spacer />
-      <router-link to="/">
-        <v-img
-          aria-label="Logo Structies"
-          contain
-          class="header-logo"
-          click
-          src="https://firebasestorage.googleapis.com/v0/b/structies.appspot.com/o/structies-logo.png?alt=media&token=14401c07-2ec3-4630-9e0d-ae70c80b4bae"
-        />
-      </router-link>
 
-      <v-spacer /> -->
-    </v-toolbar>
   </v-card>
 </template>
 
